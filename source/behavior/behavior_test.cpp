3d12b48abc0ff9a0c78132e8998c81aec7c30c2a
Lorenz Egginger <lorenz.egginger@b-plus.com>
2022-01-18 17:09:22 +0100

    Improve test example code

--- source/behavior/behavior_test.cpp
+++ source/behavior/behavior_test.cpp
@@ -2,15 +2,35 @@
 
 #include "behavior.h"
 
-// This test verifies an entire method
-TEST_CASE("testProcessTransaction", "[process_transaction_bad]")
+// It is much better to use separate tests to verify separate behaviors
+SCENARIO("a user wants to buy an article", "[process_transaction_good]")
 {
-  User user{LOW_BALANCE_THRESHOLD + dollars(2)};
-  Transaction transaction{"Pile of Beanie Babies", dollars(3)};
-  TransactionProcessor transaction_processor{};
 
-  transaction_processor.processTransaction(user, transaction);
-  REQUIRE(UI::getInstance().getText() == "You bought a Pile of Beanie Babies");
-  REQUIRE(user.getEmails().size() == 1);
-  REQUIRE(*user.getEmails().begin() == "Your balance is low");
+  GIVEN("a transaction that will take place")
+  {
+    User user{LOW_BALANCE_THRESHOLD + dollars(2)};
+    Transaction transaction{"Pile of Beanie Babies", dollars(3)};
+
+    WHEN("the user starts the transaction")
+    {
+      TransactionProcessor transaction_processor{};
+      transaction_processor.processTransaction(user, transaction);
+
+      THEN("The UI displays a notification")
+      {
+        REQUIRE(UI::getInstance().getText() == "You bought a Pile of Beanie Babies");
+      }
+
+      AND_WHEN("the user's new balance is below the threshold")
+      {
+        CHECK(user.getBalance() < LOW_BALANCE_THRESHOLD);
+
+        THEN("an email is sent to the user informing him about the low balance")
+        {
+          REQUIRE(user.getEmails().size() == 1);
+          REQUIRE(*user.getEmails().begin() == "Your balance is low");
+        }
+      }
+    }
+  }
 }
--- source/logic/logic_test.cpp
+++ source/logic/logic_test.cpp
@@ -2,22 +2,21 @@
 
 #include "logic.h"
 
-// a test that actually looks correct
-SCENARIO("navigate to photos page with logic", "[navigate_to_photos_bad]")
+// simplifying the test by inlining the variable makes the problem obvious
+SCENARIO("navigate to photos page without logic", "[navigate_to_photos_good]")
 {
 
   GIVEN("a user wants to navigate from our homepage to the photos page")
   {
-    std::string base_url = "http://plus.google.com/";
-    Navigator nav{base_url};
+    Navigator nav{"http://plus.google.com/"};
 
-    WHEN("the user clicks on a link to our photos page")
+    WHEN("the user clicks a link to our photos page")
     {
       nav.goToPhotosPage();
 
       THEN("the user gets navigated to the correct page")
       {
-        REQUIRE(nav.getCurrentUrl() == base_url + "/u/0/photos");
+        REQUIRE(nav.getCurrentUrl() == "http://plus.google.com//u/0/photos"); // fails
       }
     }
   }
